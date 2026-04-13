dc2acefe9ff81ee553f6e9890535dbe9c5021651
Daniel <amirireal@gmail.com>
2026-04-13 13:15:32 -0400

    1

---
+++ src/main/java/org/pluralsight/FullNameApplication.java
@@ -0,0 +1,21 @@
+package org.pluralsight;
+
+import java.util.Scanner;
+
+public class FullNameApplication {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your first name: ");
+        String firstName = input.nextLine();
+        System.out.println("Now enter your last name: ");
+        String lastName = input.nextLine();
+        System.out.println("Do you have an initial, if so, enter it here: ");
+        String middleInitial = input.nextLine();
+        System.out.println("Do you also have a suffix?");
+        String suffix = input.nextLine();
+
+        String fullName = firstName + " " + middleInitial + ". " + lastName + " " + suffix;
+        System.out.println("Your full name is: " + fullName.trim());
+    }
+}
