588413faeeeb8e8e774fab6108bf829ec5b36ccc
Daniel <amirireal@gmail.com>
2026-04-13 15:30:50 -0400

    2

--- src/main/java/org/pluralsight/FullNameApplication.java
+++ src/main/java/org/pluralsight/FullNameApplication.java
@@ -16,7 +16,36 @@ public class FullNameApplication {
         String suffix = input.nextLine();
 
         String fullName = firstName + " " + middleInitial + ". " + lastName + " " + suffix;
-        System.out.println("Your full name is: " + fullName.trim().replaceAll("//s+", " "));
+        String fullNameNoInitial = firstName + " " + lastName + " " + suffix;
 
+
+        switch (scannerHandlerInt()) {
+            case 1:
+                System.out.println("Your full name is: " + fullName.trim().replaceAll("//s+", " "));
+                break;
+            case 2:
+                System.out.println("Your full name is: " + fullNameNoInitial.trim().replaceAll("//s+", " "));
+                break;
+        }
+    }
+
+    public static int scannerHandlerInt() {
+        Scanner input = new Scanner(System.in);
+        int choice;
+        while (true) {
+            System.out.println("Press 1 if you want to keep your initial in the final name, otherwise press 2.");
+            if (input.hasNextInt()) {
+                choice = input.nextInt();
+                if (choice >= 1 && choice <= 2) {
+                    break;
+                } else {
+                    System.out.println("Please put in number 1 or 2.");
+                    input.next();
+                }
+            }
+            System.out.println("Please put in number 1 or 2.");
+            input.next();
+        }
+        return choice;
     }
 }
